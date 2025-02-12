1b4eec52ed2f259d3cb2dbbc90287b8c5c1d85a1
Fiza Shafiq <Fizashafiq936@gmail.com>
2025-02-12 16:41:07 +0500

    first commit

--- script.mjs
+++ script.mjs
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hy Fiza!')
+  res.send("Hy Fiza!")
 })
 
 app.listen(port,"0.0.0.0", () => {
