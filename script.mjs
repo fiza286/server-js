9d56eb3f613b6990d4dfc2fc362b48b8202694f4
Fiza Shafiq <Fizashafiq936@gmail.com>
2025-02-12 16:25:25 +0500

    Added node.mjs

---
+++ script.mjs
@@ -0,0 +1,11 @@
+import express from 'express';
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hy Fiza!')
+})
+
+app.listen(port,"0.0.0.0", () => {
+  console.log(`Example app listening on port ${port}`)
+})
